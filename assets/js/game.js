9b0b303d289dc5284fa297e72a3dcff65346c41d
Moe Yassine <moeyassine1993@gmail.com>
2022-02-13 16:53:41 -0500

    Battle the combatants until defeated - 3.2.7(half way done) - Add while loop to continuously battle enemies - Add enemy health reset in for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,46 +17,49 @@ let enemyHealth = 50
 const enemyAttack = 12
 
 const fight = function(enemyName) { 
-  window.alert("Welcome to ninja showdown!")
+  
+  while (enemyHealth > 0) {
+    const promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-  const promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-  if (promptFight.toLowerCase() === "fight") {
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    enemyHealth -= playerAttack
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
-
-    enemyHealth <= 0 ? 
-      window.alert(`${enemyName} has died`) : 
-      window.alert(`${enemyName} still has ${enemyHealth} health left`)
-
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth -= enemyAttack
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
-
-    playerHealth <= 0 ? 
-      window.alert(`${playerName} has died`) : 
-      window.alert(`${playerName} still has ${playerHealth} health left`)
-
-  } else if (promptFight.toLowerCase() === "skip") {
-    const confirmSkip = window.confirm("Are you sure you'd like to skip?")
-
-    if (confirmSkip) {
-      window.alert(`${playerName} has ran away.`)
-      playerMoney -= 2
+    if (promptFight.toLowerCase() === "fight") {
+      //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+      enemyHealth -= playerAttack
+  
+      // Log a resulting message to the console so we know that it worked.
+      console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
+  
+      enemyHealth <= 0 ? 
+        window.alert(`${enemyName} has died`) : 
+        window.alert(`${enemyName} still has ${enemyHealth} health left`)
+  
+      // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+      playerHealth -= enemyAttack
+  
+      // Log a resulting message to the console so we know that it worked.
+      console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
+  
+      playerHealth <= 0 ? 
+        window.alert(`${playerName} has died`) : 
+        window.alert(`${playerName} still has ${playerHealth} health left`)
+  
+    } else if (promptFight.toLowerCase() === "skip") {
+      const confirmSkip = window.confirm("Are you sure you'd like to skip?")
+  
+      if (confirmSkip) {
+        window.alert(`${playerName} has ran away.`)
+        playerMoney -= 2
+      } else {
+        fight()
+      }
+  
     } else {
-      fight()
+      window.alert("No valid option chosen. Try again.")
     }
-
-  } else {
-    window.alert("No valid option chosen. Try again.")
   }
 }
 
 for (i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i])
+  let pickedEnemyName = enemyNames[i]
+  enemyHealth = 50
+  fight(pickedEnemyName)
 }
