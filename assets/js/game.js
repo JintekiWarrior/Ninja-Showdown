02c096a86070ea5ec4e9901a0d28b44396754862
Moe Yassine <moeyassine1993@gmail.com>
2022-02-13 13:25:17 -0500

    Stay or GO with Control FLow - 3.1.8 - add check to see if player wants to fight or skip - add variable to keep track of player money - add penalty to player if they skip fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@
 let playerName = window.prompt("What is your name ninja?")
 let playerHealth = 100 
 const playerAttack = 10
+let playerMoney = 10
 
 console.log(playerName, playerHealth, playerAttack)
 
@@ -12,21 +13,42 @@ const enemyAttack = 12
 const fight = function() {
   window.alert("Welcome to ninja showdown!")
 
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-  enemyHealth -= playerAttack
+  const promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-  // Log a resulting message to the console so we know that it worked.
-  console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
+  if (promptFight.toLowerCase() === "fight") {
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth -= playerAttack
 
-  enemyHealth <= 0 ? 
-    window.alert(`${enemyName} has died`) : 
-    window.alert(`${enemyName} still has ${enemyHealth} health left`)
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
 
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  playerHealth -= enemyAttack
+    enemyHealth <= 0 ? 
+      window.alert(`${enemyName} has died`) : 
+      window.alert(`${enemyName} still has ${enemyHealth} health left`)
 
-  // Log a resulting message to the console so we know that it worked.
-  console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttack
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
+
+    playerHealth <= 0 ? 
+      window.alert(`${playerName} has died`) : 
+      window.alert(`${playerName} still has ${playerHealth} health left`)
+
+  } else if (promptFight.toLowerCase() === "skip") {
+    const confirmSkip = window.confirm("Are you sure you'd like to skip?")
+
+    if (confirmSkip) {
+      window.alert(`${playerName} has ran away.`)
+      playerMoney -= 2
+    } else {
+      fight()
+    }
+
+  } else {
+    window.alert("No valid option chosen. Try again.")
+  }
 }
 
 fight()
