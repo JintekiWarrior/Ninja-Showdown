c1f496b4621f97105af55ce2186e20d153ee87a8
Moe Yassine <moeyassine1993@gmail.com>
2022-02-13 11:30:15 -0500

    Manage Data with Functions, Variables, and Oper - finish 3.1.6 - add player and enemy name, health, and attack variables - add battle round functionality to fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,28 @@
 // creates a fight function
-const playerName = window.prompt("What is your name ninja?")
+let playerName = window.prompt("What is your name ninja?")
+let playerHealth = 100 
+const playerAttack = 10
 
-console.log(playerName)
+console.log(playerName, playerHealth, playerAttack)
 
-function fight() {
-  window.alert("The fight has begun!")
+const enemyName = "Death Bringer"
+let enemyHealth = 50
+const enemyAttack = 12
+
+const fight = function() {
+  window.alert("Welcome to ninja showdown!")
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth -= playerAttack
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth -= enemyAttack
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
 }
 
-// fight()
+fight()
