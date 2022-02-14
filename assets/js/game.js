9e45727d0c8d914ac2ce4029d314b63a91967e9b
Moe Yassine <moeyassine1993@gmail.com>
2022-02-14 15:45:19 -0500

    Battle until defeated using while loops - 3.2.7 - Add breaks to stop the game if enemy dies, player dies, or player skips. - Add skip feature to top of function - Add fight as the default option

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@
 
 // creates a fight function
 let playerName = window.prompt("What is your name ninja?")
-let playerHealth = 10
+let playerHealth = 100
 const playerAttack = 10
 let playerMoney = 10
 
@@ -18,46 +18,47 @@ const enemyAttack = 12
 
 const fight = function(enemyName) { 
   
-  while (enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
     const promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    if (promptFight.toLowerCase() === "fight") {
-      //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-      enemyHealth -= playerAttack
-  
-      // Log a resulting message to the console so we know that it worked.
-      console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
-  
-      enemyHealth <= 0 ? 
-        window.alert(`${enemyName} has died`) : 
-        window.alert(`${enemyName} still has ${enemyHealth} health left`)
-  
-      // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-      playerHealth -= enemyAttack
-  
-      // Log a resulting message to the console so we know that it worked.
-      console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
-  
-      if (playerHealth <= 0) {
-        window.alert(`${playerName} has died`)
-        break
-      } else {
-        window.alert(`${playerName} still has ${playerHealth} health left`)
-      }  
-  
-    } else if (promptFight.toLowerCase() === "skip") {
+    if (promptFight.toLowerCase() === "skip") {
       const confirmSkip = window.confirm("Are you sure you'd like to skip?")
   
       if (confirmSkip) {
         window.alert(`${playerName} has ran away.`)
-        playerMoney -= 2
+        playerMoney -= 10
+        console.log("player money: ", playerMoney)
+        break
       } else {
         fight()
       }
-  
+    } 
+    
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth -= playerAttack
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} hp ${enemyHealth}`)
+
+    if (enemyHealth <= 0) {
+      window.alert(`${enemyName} has died`)
+      break
     } else {
-      window.alert("No valid option chosen. Try again.")
+      window.alert(`${enemyName} still has ${enemyHealth} health left`)
     }
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth -= enemyAttack
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} hp ${playerHealth}`)
+
+    if (playerHealth <= 0) {
+      window.alert(`${playerName} has died`)
+      break
+    } else {
+      window.alert(`${playerName} still has ${playerHealth} health left`)
+    }  
   }
 }
 
